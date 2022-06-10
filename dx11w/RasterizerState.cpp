e19893c7a13c87700861b1fe01aa84ad232296e3
NULLPO-PC-2ND\nullp <nullpo.cpp@gmail.com>
2022-06-10 11:38:11 +0900

    ラスタライザステート実装

---
+++ dx11w/RasterizerState.cpp
@@ -0,0 +1,47 @@
+#include "RasterizerState.h"
+
+namespace dx
+{
+	RasterizerState::RasterizerState() : Object<ID3D11RasterizerState>()
+	{
+	}
+
+	bool RasterizerState::create(Object<ID3D11Device> &device, 
+		D3D11_FILL_MODE fillMode,
+		D3D11_CULL_MODE cullMode,
+		bool frontCounterClockwise,
+		bool multisample,
+		bool antiAliasedLine)
+	{
+		if (m_object != nullptr)
+		{
+			dx::printd(_T("rasterizer state already exists"));
+			return false;
+		}
+
+		D3D11_RASTERIZER_DESC rasterizerDescription;
+		rasterizerDescription.FillMode = fillMode;
+		rasterizerDescription.CullMode = cullMode;
+		rasterizerDescription.FrontCounterClockwise = frontCounterClockwise;
+		rasterizerDescription.DepthBias = 0;
+		rasterizerDescription.DepthBiasClamp = 0.0f;
+		rasterizerDescription.SlopeScaledDepthBias = 0.0f;
+		rasterizerDescription.DepthClipEnable = false;
+		rasterizerDescription.ScissorEnable = false;
+		rasterizerDescription.MultisampleEnable = multisample;
+		rasterizerDescription.AntialiasedLineEnable = antiAliasedLine;
+
+		auto result = device.handle()->CreateRasterizerState(&rasterizerDescription, pointer());
+		if (FAILED(result))
+		{
+			DXTRACE_ERR_MSGBOX(_T("device::createrasterizerstate"), result);
+			return false;
+		}
+		return true;
+	}
+	
+	void RasterizerState::set(Object<ID3D11DeviceContext> &deviceContext)
+	{
+		deviceContext.handle()->RSSetState(handle());
+	}
+}
---
+++ dx11w/RasterizerState.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace dx
+{
+	class RasterizerState : public Object<ID3D11RasterizerState>
+	{
+		public:
+			RasterizerState();
+
+			// 反時計回りを表面とし、背面はカリングする
+			bool create(Object<ID3D11Device> &device,
+				D3D11_FILL_MODE fillMode = D3D11_FILL_SOLID,
+				D3D11_CULL_MODE cullMode = D3D11_CULL_BACK,
+				bool frontCounterClockwise = true,
+				bool multiSample = false,
+				bool antiAliasedLine = false);
+
+			void set(Object<ID3D11DeviceContext> &deviceContext);
+	};
+}
